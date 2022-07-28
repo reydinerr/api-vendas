b7c0b53bf6bbcc024f0f80d5debaf6fb3ed4172c
redusav <reydiner@protonmail.com>
2022-07-28 14:18:00 -0300

    Criado o middleware de autenticação

---
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,28 @@
+import AppError from '@shared/errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '@config/auth';
+
+export default function isAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token is missing.');
+  }
+  // bearer {Token}
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decodedToken = verify(token, authConfig.jwt.secret);
+
+    console.log(decodedToken);
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT Token.');
+  }
+}
