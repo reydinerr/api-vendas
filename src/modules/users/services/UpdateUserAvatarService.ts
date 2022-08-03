fa9b9121041b30ced1b3d2be01e5e798fbc1b611
redusav <reydiner@protonmail.com>
2022-08-03 08:42:52 -0300

    Adicionado o serviço de upload de avatar

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,31 +1,36 @@
 import AppError from '@shared/errors/AppError';
+import path from 'path';
+import fs from 'fs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
-import { hash } from 'bcryptjs';
+import uploadConfig from '@config/upload';
 
 interface IRequest {
-  name: string;
-  email: string;
-  password: string;
+  user_id: string;
+  avatarFilename: string;
 }
 
 class UpdateUserAvatarService {
-  public async execute({ name, email, password }: IRequest): Promise<User> {
-    const usersRepository = await getCustomRepository(UsersRepository);
-    const emailExists = await usersRepository.findByEmail(email);
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
 
-    if (emailExists) {
-      throw new AppError('Email adress already exists.');
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
     }
 
-    const hashedPassword = await hash(password, 8);
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
-    const user = usersRepository.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
 
     await usersRepository.save(user);
 
