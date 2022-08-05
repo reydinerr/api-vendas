76f1d387665a70abc3e07622c58c39f211d2671c
redusav <reydiner@protonmail.com>
2022-08-04 23:59:12 -0300

    Adicionado o serviço  ResetPassword

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { isAfter, addHours } from 'date-fns';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UsersTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import { hash } from 'bcryptjs';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = await getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UsersTokensRepository);
+
+    const userToken = await userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('UserToken Not Exist');
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('UserToken Not Exist');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired.');
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
+
+export default ResetPasswordService;
