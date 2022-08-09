ab52736a4b1dff82494a5d6b5486372534eba1b7
redusav <reydiner@protonmail.com>
2022-08-08 23:50:06 -0300

    fix: refactored SendForgotPasswordEmailService

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,27 +1,38 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
-import UsersTokensRepository from '../typeorm/repositories/UserTokensRepository';
-
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 interface IRequest {
   email: string;
 }
-
 class SendForgotPasswordEmailService {
   public async execute({ email }: IRequest): Promise<void> {
-    const usersRepository = await getCustomRepository(UsersRepository);
-    const userTokenRepository = getCustomRepository(UsersTokensRepository);
-
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
     const user = await usersRepository.findByEmail(email);
-
     if (!user) {
-      throw new AppError('User Not Exist');
+      throw new AppError('User does not exists.');
     }
-    console.log(user);
 
-    const token = await userTokenRepository.generate(user.id);
-    console.log(token);
+    const { token } = await userTokensRepository.generate(user.id);
+
+    //console.log(token);
+
+    await EtherealMail.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        template: `Olá {{name}}: {{token}}`,
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
-
 export default SendForgotPasswordEmailService;
