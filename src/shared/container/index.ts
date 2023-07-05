b7f06f171a31b69d3a79779e81ebcb95c17e4a2d
reydinerr <reydiner@protonmail.com>
2023-07-05 08:58:27 -0300

    fix: correction types of IProductsRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,15 +1,9 @@
-import { ProductsRepository } from '@modules/products/infra/orm/repositories/ProductsRepository'
-import { CreateProductService } from '@modules/products/services/CreateProductService'
-import { RemoveProductService } from '@modules/products/services/RemoveProductService'
-import { ShowProductService } from '@modules/products/services/ShowProductService'
-import { UpdateProductService } from '@modules/products/services/UpdateProductService'
-import { PrismaClient } from '@prisma/client'
+import { ProductsRepository } from '@modules/products/infra/repositories/ProductsRepository'
 import { container } from 'tsyringe'
+import { IProductsRepository } from '@modules/products/domain/repositories/IProductsRepository'
 //import '@modules/products/providers'
 
-container.registerSingleton('ProductsRepository', ProductsRepository)
-container.registerSingleton('PrismaRepository', PrismaClient)
-container.registerSingleton('CreateProductService', CreateProductService)
-container.registerSingleton('UpdateProductService', UpdateProductService)
-container.registerSingleton('ShowProductService', ShowProductService)
-container.registerSingleton('ShowProductService', RemoveProductService)
+container.registerSingleton<IProductsRepository>(
+  'ProductsRepository',
+  ProductsRepository,
+)
