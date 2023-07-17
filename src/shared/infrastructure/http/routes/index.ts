7ef0a92fcf6c785e1c6bdaeef16482cca692fb28
reydinerr <reydiner@protonmail.com>
2023-07-17 15:58:23 -0300

    feat: add new profile user routes

--- src/shared/infrastructure/http/routes/index.ts
+++ src/shared/infrastructure/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import productsRouter from '@modules/products/infra/http/products.routes'
 import sessionsRouter from '@modules/user/infra/http/user.sessions.routes'
 import usersRouter from '@modules/user/infra/http/user.routes'
+import profileRouter from '@modules/user/infra/http/user.profile.routes'
 
 const routes = Router()
 
 routes.use('/products', productsRouter)
 routes.use('/session', sessionsRouter)
 routes.use('/user', usersRouter)
+routes.use('/profile', profileRouter)
 
 export default routes
