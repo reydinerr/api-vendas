37c9bbb4c76253bc8e7bcde8dcd483f7b3d46a14
reydinerr <reydiner@protonmail.com>
2023-07-17 15:55:25 -0300

    feat: add new show user unique profile service

---
+++ src/modules/user/services/ShowUserProfileService.ts
@@ -0,0 +1,23 @@
+import { injectable, inject } from 'tsyringe'
+import { NotFoundError } from '@shared/errors/AppError'
+import { UsersRepository } from '../infra/repositories/UsersRepository'
+import { IUser } from '../domain/models/IUser'
+import { IFindUserId } from '../domain/models/IFindUser'
+
+@injectable()
+export class ShowUserProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: UsersRepository,
+  ) {}
+
+  public async executeShowUser({ id }: IFindUserId): Promise<IUser> {
+    const user = await this.usersRepository.findById({ id })
+
+    if (!user) {
+      throw new NotFoundError('User not found!')
+    }
+
+    return user
+  }
+}
