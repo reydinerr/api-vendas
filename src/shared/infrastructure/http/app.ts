aa6dccc93a0f4635c4849f453725721f00403b5a
reydinerr <reydiner@protonmail.com>
2023-07-17 15:57:20 -0300

    refactor: refactor call from error middleware

--- src/shared/infrastructure/http/app.ts
+++ src/shared/infrastructure/http/app.ts
@@ -1,10 +1,10 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { errors } from 'celebrate'
 import routes from './routes'
-import AppError from '@shared/errors/AppError'
 import '@shared/container'
+import errorMiddleware from './middlewares/error'
 //import uploadConfig from '@config/upload'
 //import rateLimiter from '@shared/infra/http/middlewares/rateLimiter'
 
@@ -20,23 +20,6 @@ app.use(routes)
 
 app.use(errors())
 
-app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      })
-    }
-
-    console.log(error)
-
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    })
-  },
-)
+app.use(errorMiddleware)
 
 export { app }
