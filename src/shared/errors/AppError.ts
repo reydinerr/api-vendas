962314ea83538984a0fb1712e7d6b74b17b8088c
reydinerr <reydiner@protonmail.com>
2023-07-17 15:56:52 -0300

    feat: add new appError structure

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,11 +1,23 @@
-class AppError {
-  public readonly message: string
+export class AppError extends Error {
   public readonly statusCode: number
 
-  constructor(message: string, statusCode = 400) {
-    this.message = message
+  constructor(message: string, statusCode = 500) {
+    super(message)
     this.statusCode = statusCode
   }
 }
-
-export default AppError
+export class BadRequestError extends AppError {
+  constructor(message: string) {
+    super(message, 400)
+  }
+}
+export class NotFoundError extends AppError {
+  constructor(message: string) {
+    super(message, 404)
+  }
+}
+export class UnauthorizedError extends AppError {
+  constructor(message: string) {
+    super(message, 401)
+  }
+}
