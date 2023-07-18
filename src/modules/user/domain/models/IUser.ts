1557796762422f79a1ccc247e3a7a20c16fd35db
reydinerr <reydiner@protonmail.com>
2023-07-18 14:26:43 -0300

    feat: add new IUserReturn interface

--- src/modules/user/domain/models/IUser.ts
+++ src/modules/user/domain/models/IUser.ts
@@ -10,3 +10,15 @@ export interface IUser {
   updated_at: Date
   //getAvatarUrl(): string | null
 }
+
+export interface IUserReturn {
+  id: string
+  name: string
+  email: string
+  age: number
+  cpf: string
+  avatar: string | null
+  created_at: Date
+  updated_at: Date
+  //getAvatarUrl(): string | null
+}
