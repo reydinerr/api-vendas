53257dc02ce536f193784bdcf6fdce436504fd48
reydinerr <reydiner@protonmail.com>
2023-07-18 13:55:52 -0300

    refactor: alteration of method assignature

--- src/modules/user/domain/repositories/IUsersRepository.ts
+++ src/modules/user/domain/repositories/IUsersRepository.ts
@@ -1,4 +1,4 @@
-import { IUser } from '../models/IUser'
+import { IUser, IUserReturn } from '../models/IUser'
 import { IUpdateUserProfile } from '../models/IUpdateUserProfile'
 import { IFindUserId } from '../models/IFindUser'
 import { ICreateUser } from '../models/ICreateUser'
@@ -13,7 +13,7 @@ export interface IUsersRepository {
   create({ data }: ICreateUser): Promise<IUser>
   findById({ id }: IFindUserId): Promise<IUser | null>
   findByCpf(cpf: string): Promise<IUser | null>
-  findByEmail(email: string): Promise<IUser | null>
+  findByEmail(email: string): Promise<IUserReturn | null>
   remove(id: string): Promise<void>
   update({ id, email, password }: IUpdateUserProfile): Promise<IUser>
   getAll({ skip, take }: SearchParams): Promise<IListUser>
